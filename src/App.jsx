0e8a874547af7f6155bf877bd51583b9b6412032
SHUSANKET BASYAL <85852621+shusanket76@users.noreply.github.com>
2023-08-31 19:52:41 -0400

    routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,7 @@
-import React from 'react'
+import React from "react";
 
 const App = () => {
-  return (
-    <div>App</div>
-  )
-}
+  return <div className="bg-red-500">App</div>;
+};
 
-export default App
+export default App;
